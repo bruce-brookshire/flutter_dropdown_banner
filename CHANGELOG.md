0efdffe4792a86c2c2dbf100dba88f9bb4538532
Bruce Brookshire <bruce.t.brookshire@gmail.com>
2019-07-11 12:58:26 -0500

    Ready for release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,7 +7,7 @@
 
 * Updated SDK version to enable pub parsing
 
-## [0.1.2] - 7/11/2019
+## [1.0.0] - 7/11/2019
 
 * Example project
 * Demonstration GIF
--- test/dropdown_banner_test.dart
+++ test/dropdown_banner_test.dart
@@ -1,6 +1,6 @@
 // import 'package:flutter_test/flutter_test.dart';
 
-import 'package:dropdown_banner/dropdown_banner.dart';
+// import 'package:dropdown_banner/dropdown_banner.dart';
 
 // void main() {
 //   test('adds one to input values', () {
